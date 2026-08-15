c7da14580b0242b6cb3748a88e9a8cdc5500bbf0
Oliver _ <firebirdx14@gmail.com>
2026-08-15 03:25:06 +0000

    File changes

--- src/components/rfq/BrandHeader.jsx
+++ src/components/rfq/BrandHeader.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
-import { Mail } from "lucide-react";
+
+const LIT_LOGO = "https://media.base44.com/images/public/6a7fda35ce3693e5050df072/e87f0cfc0_lit_logo_white.svg";
 
 export default function BrandHeader() {
   return (
     <header className="rfq-header">
       <Link to="/" className="rfq-brand">
-        <span className="rfq-logo"><Mail size={21} /></span>
-        <span><strong>RFQ</strong> WATCH<small>Government opportunities. Delivered.</small></span>
+        <img src={LIT_LOGO} alt="LIT" className="rfq-lit-logo" />
+        <span><strong>RFQ Watch</strong> <small>Lite</small></span>
       </Link>
     </header>
   );
